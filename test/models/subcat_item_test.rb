86f51ae672f3331b1a0f42f9fa53062b70047f79
Edward Cho <eycho@andrew.cmu.edu>
2019-10-08 09:39:53 -0400

    Added validation testing to subcat items

--- test/models/subcat_item_test.rb
+++ test/models/subcat_item_test.rb
@@ -26,13 +26,32 @@ class SubcatItemTest < ActiveSupport::TestCase
     end
 
     # test the scope for item
-    should "have a scope to list the saved items for a given item" do
+    should "have a scope to list the subcat items for a given item" do
       assert_equal [@si1, @si5], SubcatItem.for_item(@item2).all.sort
     end
 
-    # test the scope for user
-    should "have a scope to list the saved items for a given subcat" do
+    # test the scope for subcat
+    should "have a scope to list the subcat items for a given subcat" do
       assert_equal [@si1, @si2, @si3, @si4], SubcatItem.for_subcat(@subcat1).all.sort
+    end
+
+
+    # test custom validation subcat is active in the system
+    should "only allow subcat items to be created when the subcat is active and in the system" do
+      @badsi1 = FactoryBot.build(:subcat_item, subcat: @subcat10, item: @item2)
+      deny @badsi1.valid?
+      @badsubcat = FactoryBot.build(:subcat, category: @cat6, name: 'Bad')
+      @badsi2 = FactoryBot.build(:subcat_item, subcat: @badsubcat, item: @item2)
+      deny @badsi2.valid?
+    end
+
+    # test custom validation item is active in the system
+    should "only allow subcat items to be created when the item is active and in the system" do
+      @badsi1 = FactoryBot.build(:subcat_item, subcat: @subcat5, item: @item10)
+      deny @badsi1.valid?
+      @baditem = FactoryBot.build(:item, title: 'baditem', link: 'baditem.com')
+      @badsi2 = FactoryBot.build(:subcat_item, subcat: @subcat5, item: @baditem)
+      deny @badsi2.valid?
     end
 
   end
