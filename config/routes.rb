402801bd2abfddad174878f590e9ab415ba4af3a
Edward Cho <eycho@andrew.cmu.edu>
2019-10-21 22:29:01 -0400

    Added home controller, routes, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   
+  # Semi-static page routes
+  get 'home', to: 'home#index', as: :home
+  get 'home/about', to: 'home#about', as: :about
+  get 'home/contact', to: 'home#contact', as: :contact
+  get 'home/privacy', to: 'home#privacy', as: :privacy
+  get 'home/search', to: 'home#search', as: :search
+
   resources :categories
   resources :items
   resources :subcats
@@ -7,5 +14,7 @@ Rails.application.routes.draw do
   resources :starred_items
   resources :subcat_items
   
+  # You can have the root of your site routed with 'root'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
